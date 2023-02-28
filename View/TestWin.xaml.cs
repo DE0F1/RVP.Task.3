f967992969b91c6fdd99c007918bbc73af861d73
LynxAG0 <vanya.popkin.10@gmail.com>
2023-02-28 09:01:20 +0300

    Добавьте файлы проекта.

---
+++ View/TestWin.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Windows;
+
+
+namespace RVP.Task._3.View
+{
+    /// <summary>
+    /// Логика взаимодействия для TestWin.xaml
+    /// </summary>
+    public partial class TestWin : Window
+    {
+        public TestWin()
+        {
+            InitializeComponent();
+        }
+    }
+}
